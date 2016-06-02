c5395e509f71f578b80edad95705f56e00c509b2
Tony <tony@gmail.com>
2016-06-02 16:40:58 +0800

    Main alter complete but hbase.client is error

--- src/main/java/com/suda/cs/Main.java
+++ src/main/java/com/suda/cs/Main.java
@@ -1,7 +1,61 @@
 package com.suda.cs;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 /**
  * Created by fei on 2016/6/2.
  */
 public class Main {
-}
+	public static int main(String[] args) {
+		if(args.length !=1){
+            System.out.println("xml file is not one, you should provide only one xml file");
+            return 0;
+        }
+		String fileName = args[0];		
+		ParseXml.parserXml(fileName);
+		Configuration configuration = new Configuration();
+        configuration.set("data.seperator", ParseXml.fieldSeparator);
+        configuration.set("hbase.table.name", ParseXml.table.getName());
+
+        Job job = Job.getInstance(configuration, "Bulk Loading HBase Table::" + ParseXml.table.getName());
+        job.setJarByClass(Main.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setMapOutputKeyClass(ImmutableBytesWritable.class);//指定输出键类
+        job.setMapOutputValueClass(Put.class);//指定输出值类
+        job.setMapperClass(BulkLoadMapper.class);//指定Map函数
+        FileInputFormat.addInputPaths(job, ParseXml.inputPath);//输入路径
+        FileSystem fs = FileSystem.get(configuration);
+        Path output = new Path(ParseXml.inputPath+"output");
+        if (fs.exists(output)) {
+            fs.delete(output, true);//如果输出路径存在，就将其删除
+        }
+        FileOutputFormat.setOutputPath(job, output);//输出路径
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        TableName tableName = TableName.valueOf(ParseXml.table.getName());
+        HFileOutputFormat2.configureIncrementalLoad(job, connection.getTable(tableName), connection.getRegionLocator(tableName));
+        job.waitForCompletion(true);
+        if (job.isSuccessful()){
+            HFileLoader.doBulkLoad(output.toString(), ParseXml.table.getName());//导入数据
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+		
+	}
+	
+
